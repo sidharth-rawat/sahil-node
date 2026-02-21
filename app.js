86b1e1eddc612e453396502e7f76ac5418da78e5
sidharth-rawat <whosidrawat69@gmail.com>
2026-02-21 13:53:02 +0530

    fix route

--- app.js
+++ app.js
@@ -4,12 +4,29 @@ const AppError = require("./utils/AppError");
 const globalError = require("./middlewares/globalError");
 const todoRoutes = require("./routes/todoRoutes")
 const cors= require("cors")
+const { checkConnection } = require("./config/db")
 const app = express();
 
 app.use(express.json())
 app.use(cors())
 app.use("/api/v1/users", authRoutes)
 app.use("/api/v1/todo", todoRoutes)
+
+app.get("/app", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Server is running 🚀"
+    });
+});
+
+app.get("/health", async (req, res) => {
+    try {
+        const healthStatus = await checkConnection();
+        return res.status(200).json(healthStatus);
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+});
 
 app.use((req, res, next) => {
     next(new AppError(`path not found ${req.originalUrl}`, 404));
--- server.js
+++ server.js
@@ -2,28 +2,9 @@ require("dotenv").config({path:".env", quiet:true})
 
 
 const app = require("./app");
-const {connectDB,checkConnection} = require("./config/db");
+const { connectDB } = require("./config/db");
 const port = process.env.PORT;
 
-
-app.get('/health', async (req, res) => {
-    try {
-        const healthStatus = await checkConnection();
-        return res.status(200).json(healthStatus);
-    } catch (error) {
-        return res
-            .status(400)
-            .json({ message: error.message });
-    }
-});
-
-app.get("/app", (req, res) => {
-    console.log("Route hit");
-    res.status(200).json({
-        success: true,
-        message: "Server is running 🚀"
-    });
-});
 
 
 connectDB();
