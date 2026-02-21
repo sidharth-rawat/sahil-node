2c4ae0219b83cf0a9510722e45e7bf61b4f2b0d7
sidharth-rawat <whosidrawat69@gmail.com>
2026-02-21 13:08:27 +0530

    update route endpoint

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ app.use(cors())
 app.use("/api/v1/users", authRoutes)
 app.use("/api/v1/todo", todoRoutes)
 
-app.all("*", (req, res, next) => {
+app.use((req, res, next) => {
     next(new AppError(`path not found ${req.originalUrl}`, 404));
 });
+
 
 
 app.use(globalError)
